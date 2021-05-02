44bf71178a0fcb792d0534befe87e371aaa79399
Ata-92 <atakal92@gmail.com>
2021-05-02 21:04:18 +0300

    Add Card component

---
+++ tour-places/src/components/card/Card.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./Card.css";
+import { data } from "../../helper/data";
+
+class Card extends Component {
+
+  render() {
+
+    return (
+      <div className="Card">
+        {data.map((i) => (
+          <div className={`card${i.id}`} onMouseOver={this.hover}>
+            <h2 className={`title${i.id}`}>{i.title}</h2>
+            <img src={i.image} className={`image${i.id}`} alt="" />
+            <p className={`desc${i.id}`} onMouseOver={this.hover}>{i.desc}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Card;
