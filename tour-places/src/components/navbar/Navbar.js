a040a853fb039fed01c5c63a25fec40b468ba7f1
Ata-92 <atakal92@gmail.com>
2021-07-03 16:26:33 +0300

    Fix nav links bug

--- tour-places/src/components/navbar/Navbar.js
+++ tour-places/src/components/navbar/Navbar.js
@@ -4,12 +4,12 @@ const Navbar = () => {
   return (
     <div className="Navbar">
       <nav className="links">
-        <a href="aboutus.html">ABOUT US</a>
-        <a href="foryou">FOR YOU</a>
-        <a href="services">SERVICES</a>
-        <a href="blog">BLOG</a>
-        <a href="vlog">VLOG</a>
-        <a href="contact">CONTACT</a>
+        <a href="/aboutus">ABOUT US</a>
+        <a href="/foryou">FOR YOU</a>
+        <a href="/services">SERVICES</a>
+        <a href="/blog">BLOG</a>
+        <a href="/vlog">VLOG</a>
+        <a href="/contact">CONTACT</a>
       </nav>
     </div>
   );
